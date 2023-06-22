9c2183b061703f8f55641f15fd8eed6a5f342bad
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2023-06-22 10:32:25 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,8 +6,50 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:19:05 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/06/12 13:23:40 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/06/15 03:30:04 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int  checklen(long int i)
+{
+    int total;
+
+    total = 0;
+    if (i < 0)
+    {
+        i = i * -1;
+        total++;
+    }
+    if (i > 0)
+    {   
+        i = i / 10;
+        i++;
+    }
+    return (total);
+}
+char    ft_itoa(int c)
+{
+    char    *array;
+    int    i;
+    long int    nb;
+
+    nb = c;
+    i = checklen(nb);
+    array = (char *)malloc(i * sizeof(char) + 1);
+    if (!array)
+        return 0;
+    if (nb < 0)
+    {
+        array[0] = '-';
+        nb = nb * -1;
+    }
+    while (nb > 0)
+    {
+        array[i] = nb % 10 + '0';
+        nb = nb / 10;
+        i--;
+    }
+    return ((char)array);        
+}
