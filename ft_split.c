bdab66f29626dd25c8a8b5a4f6a197d2347068dd
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2023-06-27 12:31:52 +0200

    mas cerca del split

--- ft_split.c
+++ ft_split.c
@@ -3,27 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbotargu <pbotargu@student.42barcel>       +#+  +:+       +#+        */
+/*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/22 14:47:28 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/06/22 15:24:29 by pbotargu         ###   ########.fr       */
+/*   Created: 2023/06/15 02:49:34 by pbotargu          #+#    #+#             */
+/*   Updated: 2023/06/15 02:54:49 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static size_t	count_words(const char *s, char c)
+#include "libft.h"
+
+int checkwords(char const *s, char c)
 {
-	size_t	i;
-	int		x;
+    int numwords = 0;
+    int i = 0;
 
-	x = 0;
-	while(s[x])
-	{
-		while (s[x] == c)
-			x++;
-
-	}
+    while (s[i] != '\0')
+    {
+        while (s[i] == c)
+            i++;
+        if (s[i] != c && s[i] != '\0')
+        {
+            numwords++;
+            while (s[i] != c && s[i] != '\0')
+                i++;
+        }
+    }
+    return numwords;
 }
+
 char **ft_split(char const *s, char c)
 {
-	
+    char **split;
+    int i;
+    int first;
+    int last;
+
+    split = (char **)malloc((checkwords(s,c) + 1) * sizeof(char *));
+    if (!split)
+        return NULL;
+    i = 0;
+    while (*s)
+    {
+        first = 0;
+        last = 0;
+        while (*s == c)
+            s++;
+        while (*s != c && *s != '\0')
+        {
+            s++;
+            last++;
+        }
+        split[i] = ft_substr(s - last, 0, last);
+        i++;
+    }
+    split[i] = NULL; 
+    return split;
 }
